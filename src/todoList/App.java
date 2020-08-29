b59841afba7393d283262e0a2976ff8e3b1d16a2
Jason Gasior <jasongasior@live.com>
2020-08-29 16:46:42 -0500

    added constuctor and priority to task

--- src/todoList/App.java
+++ src/todoList/App.java
@@ -3,5 +3,6 @@ import todoList.Task;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Task task = new Task("Test Task :)");
     }
 }
--- src/todoList/Task.java
+++ src/todoList/Task.java
@@ -1,4 +1,16 @@
 package todoList;
 public class Task{
-    
+    String taskName = "";
+    //date
+    int priority = 4;//default priority | lowest prio 1 is most important
+
+    Task(String name){
+        this.taskName = name;
+
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
 }
