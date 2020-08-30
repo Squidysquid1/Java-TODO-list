0f43d1ad1af495380fbff52b2747c7e24f699a4e
Jason Gasior <jasongasior@live.com>
2020-08-30 14:11:23 -0500

    Furthering development

--- src/todoList/Task.java
+++ src/todoList/Task.java
@@ -1,7 +1,9 @@
 package todoList;
-public class Task{
+import java.time.LocalDate;
+
+public class Task {
     String taskName = "";
-    //date
+    LocalDate date;
     int priority = 4;//default priority | lowest prio 1 is most important
 
     Task(String name){
@@ -10,7 +12,23 @@ public class Task{
     }
 
     public void setPriority(int priority) {
-        this.priority = priority;
+        if(priority < 1 || priority > 4)
+            System.out.println("Cant set a priority not in the range 1-4");
+        else
+            this.priority = priority;
     }
+    
+    public void setDate(String date){//year-month-date
+        this.date= LocalDate.parse(date) ;
+
+    }
+
+    public void addLabel(String labelName){
+
+    }
+    public void deleteLabel(String labelName){
+
+    }
+
 
 }
--- src/todoList/UserTODOmanager.java
+++ src/todoList/UserTODOmanager.java
@@ -1,5 +1,16 @@
 package todoList;
+import java.util.ArrayList;
 
 public class UserTODOmanager {
-    
+    ArrayList taskStorageArray = new ArrayList();
+    public void addNewTask(String taskName){
+        taskStorageArray.add(new Task(taskName));
+    }
+    public void deleteTask(int index/*have either int for index or a task name for a hashmap*/){
+        int arrSize = taskStorageArray.size();
+        if(index<0 || index > arrSize)
+            System.out.println("Array index out of bounds for delete task");
+        else
+            taskStorageArray.remove(index);
+    }
 }
